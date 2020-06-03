ac1e80dd9b0a4d3176345383aa613cba06218977
JoshuaZZZs <15979089154@189.cn>
2020-06-03 18:16:07 +0800

    update list page

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,5 +1,5 @@
 // pages/list/list.js
-const app=getApp()
+const app = getApp()
 Page({
 
   /**
@@ -7,31 +7,60 @@ Page({
    */
   data: {
     //页面信息
-    pageInfo:{},
+    pageInfo: {},
     //列表数据
-    listData:[]
+    listData: [],
+    //是否还有更多数据
+    hasMore:true,
+    //页面初始数据
+    pageNo:1,
+    pageSize:15
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.getPageInfo(options)
-    this.getListData(options)
   },
-  getPageInfo(options){
-    app.fetch(`/categories/${options.cat}`).then(res=>{
-                 this.setData({ pageInfo:res.data})
-                 wx.setNavigationBarTitle({
-                  title: res.data.name,
-                })
-        })
+  onReady() {
+    if (this.data.pageInfo.name) {
+      wx.setNavigationBarTitle({
+        title: this.data.pageInfo.name,
+      })
+    }
   },
-
-  getListData(options){
-    app.fetch(`/categories/${options.cat}/shops`).then(res=>{     
-        this.setData({listData:res.data})
-    
-       })
+  onReachBottom(){
+    if (this.data.hasMore) {
+      this.getListData()
+    }
+  },
+  getPageInfo(options) {
+    app.fetch(`/categories/${options.cat}`).then(res => {
+      this.setData({
+        pageInfo: res.data
+      })
+      wx.setNavigationBarTitle({
+        title: res.data.name,
+      })
+      this.getListData(options)
+    })
+  },
+  getListData(options=this.data.pageInfo.id) {
+    debugger
+    const param={
+      _page:this.data.pageNo,
+      _limit:this.data.pageSize
+    }
+    app.fetch(`/categories/${options.cat}/shops`,param).then(res => {
+      debugger
+      const hasMore=this.data.pageNo*this.data.pageSize<=parseInt(res.header['x-total-count'])?true:false
+      this.setData({
+        listData:this.data.listData.concat(res.data),
+        hasMore:hasMore,
+        pageNo:hasMore?this.data.pageNo++:this.data.pageNo,
+      })
+      
+    })
   }
 
 })
